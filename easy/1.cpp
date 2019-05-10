e57ba26f7693ee8742b4403595c52bb582fb3494
root <root@DESKTOP-3M1NNNC.localdomain>
2019-05-09 23:15:37 -0700

    first

---
+++ easy/1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int first, second;
+        for(int i = 0; i< nums.size(); i++){
+           for (int j = i; j< nums.size();j++){
+               if(nums[i] + nums[j] == target && i!=j){
+                   first = i;
+                   second = j;
+               }
+           }
+        }
+        vector<int> a = {first,second};
+        return a;
+    }
+};
+
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// Example:
+//
+// Given nums = [2, 7, 11, 15], target = 9,
+//
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
