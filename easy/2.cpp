58d251598a283dfb8550c0041fc86e9ec67390df
root <root@DESKTOP-3M1NNNC.localdomain>
2019-05-19 13:30:25 -0700

    update

---
+++ easy/2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead  = new ListNode(0);
+        ListNode* p = l1, *q = l2, *curr = dummyHead;
+        int carry = 0;
+        while(p != NULL || q!= NULL){
+            int x = (p != NULL )? p->val : 0;
+            int y = (q != NULL )? q->val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+
+            curr -> next = new ListNode(sum % 10);
+            curr = curr -> next;
+            if(p != NULL) p = p -> next;
+            if(q != NULL) q = q -> next;
+        }
+        if(carry > 0){
+            curr -> next = new ListNode(carry);
+        }
+        return dummyHead -> next;
+    }
+};
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//
+// Example:
+//
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
